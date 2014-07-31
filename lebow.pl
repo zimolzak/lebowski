d2945584e195dd79e3136b24a53a6265b6388d93
Andrew Zimolzak <andyzimolzak@gmail.com>
2014-07-31 14:52:59 -0400

    command line switches

--- lebow.pl
+++ lebow.pl
@@ -1,10 +1,18 @@
 #!/usr/bin/perl -w
 use strict;
 
-while(<>){
+my $Input_filename = shift;
+open(INPUT, "< $Input_filename") or die "Can't open $Input_filename";
+
+my $Ngrams = 1;
+while (my $_ = shift) {
+    $Ngrams = shift if /--ngrams/;
+}
+
+while(<INPUT>){
     s/^\s*//g;
     s/(\S+)\s+/\L$1\n/g; # lowercase everything & add newlines
     s/--/\n/g;
-    s/[.,:"?!]//g;
+    s/[.,:"?!();]//g;
     print;
 }
