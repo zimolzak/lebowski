f5c5e9ec95a610429fab853f7076b73b783bacdd
Andrew Zimolzak <andyzimolzak@gmail.com>
2014-12-21 15:37:57 -0500

    working split of dialog into array of lines

--- dialog_only.pl
+++ dialog_only.pl
@@ -1,5 +1,18 @@
 #!/usr/bin/perl -w
 use strict;
+my @dialog;
+my @this_speech;
 while(<>){
-    print if /^\t\t\t\t[^\t]/ or not /\t/;
+    chomp;
+    s/\r//g;
+    if (s/^\t\t\t\t([^\t])/$1/){
+	push (@this_speech, $_) ;
+    }
+    else {
+	push (@dialog, join(" ", @this_speech));
+	#print join(" ", @this_speech) . "\n\n";
+	undef @this_speech;
+	next;
+    }
 }
+print join("\n\n", @dialog);
